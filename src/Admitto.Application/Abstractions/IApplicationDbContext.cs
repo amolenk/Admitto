8ba93912df8f11c7fbb4818a47c5cefcdee6766d
Sander Molenkamp <sander@sandermolenkamp.com>
2025-03-03 07:39:09 +0100

    Move DbContext implementation to infrastructure

---
+++ src/Admitto.Application/Abstractions/IApplicationDbContext.cs
@@ -0,0 +1,11 @@
+using Amolenk.Admitto.Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+
+namespace Amolenk.Admitto.Application.Abstractions;
+
+public interface IApplicationDbContext
+{
+    DbSet<TicketedEvent> TicketedEvents { get; }
+    
+    Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+}
--- src/Admitto.Application/Data/ApplicationDbContext.cs
+++
@@ -1,9 +0,0 @@
-using Amolenk.Admitto.Domain.Entities;
-using Microsoft.EntityFrameworkCore;
-
-namespace Amolenk.Admitto.Application.Data;
-
-public class ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : DbContext(options)
-{
-    public DbSet<TicketedEvent> TicketedEvents { get; set; } = null!;
-}
