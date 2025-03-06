9bc5dbff06dc6a252ccd6bf4e44c5442ed69d90d
Sander Molenkamp <sander@sandermolenkamp.com>
2025-03-06 17:17:36 +0100

    Fix arg

--- src/Admitto.Application/UseCases/TicketedEvents/GetTicketedEvent/GetTicketedEventHandler.cs
+++ src/Admitto.Application/UseCases/TicketedEvents/GetTicketedEvent/GetTicketedEventHandler.cs
@@ -9,7 +9,7 @@ public class GetTicketedEventHandler(IApplicationContext context)
     public async ValueTask<GetTicketedEventResult?> HandleAsync(GetTicketedEventQuery query, 
         CancellationToken cancellationToken)
     {
-        var ticketedEvent = await context.TicketedEvents.FindAsync([query.Id, cancellationToken],
+        var ticketedEvent = await context.TicketedEvents.FindAsync([query.Id],
             cancellationToken: cancellationToken);
 
         return ticketedEvent is not null ? GetTicketedEventResult.FromTicketedEvent(ticketedEvent) : null;
