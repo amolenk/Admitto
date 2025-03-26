829e8c608b6f94247f17a9903d380203e7d4d9bf
Sander Molenkamp <sander@sandermolenkamp.com>
2025-03-26 09:51:21 -0700

    Add endpoint for adding team

--- src/Admitto.Application/UseCases/Teams/CreateOrganizingTeam/CreateOrganizingTeamCommand.cs
+++
@@ -1,8 +0,0 @@
-namespace Amolenk.Admitto.Application.UseCases.Teams.CreateOrganizingTeam;
-
-public record CreateOrganizingTeamCommand(string Name) : ICommand
-{
-    public Guid Id { get; } = Guid.NewGuid();
-}
-
-public record CreateOrganizingTeamResult(Guid Id);
