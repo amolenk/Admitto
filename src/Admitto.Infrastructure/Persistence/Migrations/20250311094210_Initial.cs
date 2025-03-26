6a409222ea0fdce2dba8b69966bfb09743be3530
Sander Molenkamp <sander@sandermolenkamp.com>
2025-03-26 08:34:08 -0700

    WIP

---
+++ src/Admitto.Infrastructure/Persistence/Migrations/20250311094210_Initial.cs
@@ -0,0 +1,180 @@
+﻿using System;
+using System.Text.Json;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Amolenk.Admitto.Infrastructure.Persistence.Migrations
+{
+    /// <inheritdoc />
+    public partial class Initial : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "attendee_activities",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    attendee_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    activity = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    timestamp = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_attendee_activities", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "attendee_registrations",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    event_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    email = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    first_name = table.Column<string>(type: "character varying(32)", maxLength: 32, nullable: false),
+                    last_name = table.Column<string>(type: "character varying(32)", maxLength: 32, nullable: false),
+                    organization_name = table.Column<string>(type: "character varying(32)", maxLength: 32, nullable: false),
+                    status = table.Column<int>(type: "integer", nullable: false),
+                    ticket_order = table.Column<string>(type: "jsonb", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_attendee_registrations", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "authorization_codes",
+                columns: table => new
+                {
+                    code = table.Column<Guid>(type: "uuid", nullable: false),
+                    user_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    code_challenge = table.Column<string>(type: "character varying(32)", maxLength: 32, nullable: false),
+                    expires = table.Column<DateTime>(type: "timestamp with time zone", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_authorization_codes", x => x.code);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "email_messages",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    ticketed_event_id = table.Column<Guid>(type: "uuid", nullable: true),
+                    template_id = table.Column<string>(type: "text", nullable: false),
+                    recipient_email = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    priority = table.Column<bool>(type: "boolean", nullable: false),
+                    template_parameters = table.Column<string>(type: "jsonb", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_email_messages", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "organizing_teams",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    name = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    members = table.Column<string>(type: "jsonb", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_organizing_teams", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "outbox",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    data = table.Column<JsonDocument>(type: "jsonb", nullable: false),
+                    type = table.Column<string>(type: "character varying(255)", maxLength: 255, nullable: false),
+                    priority = table.Column<bool>(type: "boolean", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_outbox", x => x.id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "refresh_tokens",
+                columns: table => new
+                {
+                    token = table.Column<Guid>(type: "uuid", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_refresh_tokens", x => x.token);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "team_members",
+                columns: table => new
+                {
+                    team_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    user_id = table.Column<Guid>(type: "uuid", nullable: false),
+                    email = table.Column<string>(type: "character varying(50)", maxLength: 50, nullable: false),
+                    role = table.Column<int>(type: "integer", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_team_members", x => new { x.team_id, x.user_id });
+                });
+
+            migrationBuilder.CreateTable(
+                name: "ticketed_events",
+                columns: table => new
+                {
+                    id = table.Column<Guid>(type: "uuid", nullable: false),
+                    name = table.Column<string>(type: "character varying(100)", maxLength: 100, nullable: false),
+                    start_day = table.Column<DateOnly>(type: "date", nullable: false),
+                    end_day = table.Column<DateOnly>(type: "date", nullable: false),
+                    sales_start_date_time = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    sales_end_date_time = table.Column<DateTime>(type: "timestamp with time zone", nullable: false),
+                    ticket_types = table.Column<string>(type: "jsonb", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ticketed_events", x => x.id);
+                });
+            
+            migrationBuilder.AddOutboxReplication();
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "attendee_activities");
+
+            migrationBuilder.DropTable(
+                name: "attendee_registrations");
+
+            migrationBuilder.DropTable(
+                name: "authorization_codes");
+
+            migrationBuilder.DropTable(
+                name: "email_messages");
+
+            migrationBuilder.DropTable(
+                name: "organizing_teams");
+
+            migrationBuilder.DropTable(
+                name: "outbox");
+
+            migrationBuilder.DropTable(
+                name: "refresh_tokens");
+
+            migrationBuilder.DropTable(
+                name: "team_members");
+
+            migrationBuilder.DropTable(
+                name: "ticketed_events");
+        }
+    }
+}
