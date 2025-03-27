701420798d38f37cc3f55dea1500be6a26e6d639
Sander Molenkamp <sander@sandermolenkamp.com>
2025-03-27 11:05:00 -0700

    Remove custom auth use cases

--- src/Admitto.Application/UseCases/Auth/SendMagicLink/SendMagicLinkCommand.cs
+++
@@ -1,6 +0,0 @@
-namespace Amolenk.Admitto.Application.UseCases.Auth.SendMagicLink;
-
-public record SendMagicLinkCommand(string Email, string CodeChallenge) : ICommand
-{
-    public Guid Id { get; } = Guid.NewGuid();
-}
