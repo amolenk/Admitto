8c5aed87cd34f19bfc119d79de0b7d4bd2ee46c9
Sander Molenkamp <sander@sandermolenkamp.com>
2025-03-27 11:05:00 -0700

    Remove custom auth use cases

--- src/Admitto.Application/UseCases/Auth/GetApplicationToken/GetApplicationTokenCommand.cs
+++
@@ -1,6 +0,0 @@
-// namespace Amolenk.Admitto.Application.UseCases.Accounts.GetApplicationToken;
-//
-// public record GetApplicationTokenCommand(string ClientId, string ClientSecret) : ICommand
-// {
-//     public Guid CommandId { get; } = Guid.NewGuid();
-// }
--- src/Admitto.Application/UseCases/Auth/RefreshToken.cs
+++
@@ -1,9 +0,0 @@
-namespace Amolenk.Admitto.Application.UseCases.Auth;
-
-/// <summary>
-/// Represents a refresh token in an OAuth2 flow.
-/// </summary>
-public class RefreshToken
-{
-    public required Guid Token { get; set; } = Guid.NewGuid();
-}
