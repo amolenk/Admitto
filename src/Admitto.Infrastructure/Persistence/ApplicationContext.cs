7113bc546082df25e4f63bedf2fbe617c865592a
Sander Molenkamp <sander@sandermolenkamp.com>
2025-03-27 11:05:57 -0700

    Remove auth from EF Core

--- src/Admitto.Infrastructure/Persistence/ApplicationContext.cs
+++ src/Admitto.Infrastructure/Persistence/ApplicationContext.cs
@@ -1,7 +1,6 @@
 using System.Reflection;
 using Amolenk.Admitto.Application.Common.Abstractions;
 using Amolenk.Admitto.Application.ReadModel.Views;
-using Amolenk.Admitto.Application.UseCases.Auth;
 using Amolenk.Admitto.Application.UseCases.Email;
 using Amolenk.Admitto.Application.UseCases.Email.SendEmail;
 using Amolenk.Admitto.Domain.Entities;
@@ -10,20 +9,15 @@ using Microsoft.EntityFrameworkCore;
 namespace Amolenk.Admitto.Infrastructure.Persistence;
 
 public class ApplicationContext(DbContextOptions options) : DbContext(options), IDomainContext, IReadModelContext,
-    IAuthContext, IEmailContext, IEmailOutbox, IMessageOutbox, IUnitOfWork
+    IEmailContext, IEmailOutbox, IMessageOutbox, IUnitOfWork
 {
     // IDomainContext sets
     public DbSet<AttendeeRegistration> AttendeeRegistrations { get; set; } = null!;
     public DbSet<Team> Teams { get; set; } = null!;
-//    public DbSet<TicketedEvent> TicketedEvents { get; set; } = null!;
     
     // IReadModelContext sets
     public DbSet<AttendeeActivityView> AttendeeActivities { get; set; } = null!;
     public DbSet<TeamMembersView> TeamMembers { get; set; } = null!;
-    
-    // IAuthContext sets
-    public DbSet<AuthorizationCode> AuthorizationCodes { get; set; } = null!;
-    public DbSet<RefreshToken> RefreshTokens { get; set; } = null!;
     
     // IEmailContext sets
     public DbSet<EmailMessage> EmailMessages { get; set; } = null!;
