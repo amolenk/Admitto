1a5e93d5efa4c4c85ca627474159909b000dac0b
Sander Molenkamp <sander@sandermolenkamp.com>
2025-06-13 16:13:22 +0200

    Add validations

--- src/Admitto.Application/UseCases/Teams/CreateTeam/CreateTeamValidator.cs
+++ src/Admitto.Application/UseCases/Teams/CreateTeam/CreateTeamValidator.cs
@@ -1,3 +1,5 @@
+using Amolenk.Admitto.Domain.ValueObjects;
+
 namespace Amolenk.Admitto.Application.UseCases.Teams.CreateTeam;
 
 public class CreateTeamValidator : AbstractValidator<CreateTeamRequest>
@@ -5,8 +7,48 @@ public class CreateTeamValidator : AbstractValidator<CreateTeamRequest>
     public CreateTeamValidator()
     {
         RuleFor(x => x.Name)
+            .NotNull().WithMessage("Team name must be set.")
             .MinimumLength(2).WithMessage("Team name must be at least 2 characters long.")
-            .MaximumLength(100).WithMessage("Team name must be 100 characters or less.")
+            .MaximumLength(50).WithMessage("Team name must be 50 characters or less.")
             .OverridePropertyName("name");
+
+        RuleForEach(x => x.Members)
+            .ChildRules(member =>
+            {
+                member.RuleFor(m => m.Email)
+                    .NotNull().WithMessage("Member email must be set.")
+                    .EmailAddress().WithMessage("Member email must be a valid email address.")
+                    .OverridePropertyName("email");
+
+                member.RuleFor(m => m.Role)
+                    .Must(TeamMemberRole.IsValid)
+                    .WithMessage($"Member role must be one of {string.Join(", ", TeamMemberRole.ValidRoles)}.")
+                    .OverridePropertyName("role");
+            })
+            .OverridePropertyName("members");
+
+        RuleFor(x => x.EmailSettings)
+            .NotNull()
+            .WithMessage("Email settings must be set.")
+            .OverridePropertyName("emailSettings");
+
+        // ReSharper disable once ConditionIsAlwaysTrueOrFalseAccordingToNullableAPIContract
+        When(x => x.EmailSettings is not null, () =>
+        {
+            RuleFor(x => x.EmailSettings.SenderEmail)
+                .NotNull().WithMessage("Sender email must be set.")
+                .EmailAddress().WithMessage("Sender email must be a valid email address.")
+                .OverridePropertyName("senderEmail");
+
+            RuleFor(x => x.EmailSettings.SmtpServer)
+                .NotEmpty().WithMessage("SMTP server must not be empty.")
+                .MaximumLength(20).WithMessage("SMTP server must be 50 characters or less.")
+                .OverridePropertyName("smtpServer");
+
+            RuleFor(x => x.EmailSettings.SmtpPort)
+                .InclusiveBetween(1, 65535).WithMessage("SMTP port must be between 1 and 65535.")
+                .OverridePropertyName("smtpPort");
+
+        });
     }
 }
