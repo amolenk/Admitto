95bb147cdda224b87df4c945e669a3433bc788da
Sander Molenkamp <sander@sandermolenkamp.com>
2025-07-28 14:15:12 +0200

    WIP

--- src/Admitto.ServiceDefaults/Extensions.cs
+++ src/Admitto.ServiceDefaults/Extensions.cs
@@ -194,18 +194,20 @@ public static class Extensions
 
     public static TBuilder AddDataProtection<TBuilder>(this TBuilder builder) where TBuilder : IHostApplicationBuilder
     {
-        var connectionString = builder.Configuration.GetConnectionString("blobs");
-        if (!string.IsNullOrEmpty(connectionString))
-        {
-            var blobClient = new BlobContainerClient(connectionString, "data-protection")
-                .GetBlobClient("keys.xml");
-
-            builder.Services.AddDataProtection()
-                .PersistKeysToAzureBlobStorage(blobClient)
-                .SetApplicationName("Admitto");
+        // TODO Remove is this works locally. In prod, there keys are now managed automatically.
+        
+        // var connectionString = builder.Configuration.GetConnectionString("blobs");
+        // if (!string.IsNullOrEmpty(connectionString))
+        // {
+        //     var blobClient = new BlobContainerClient(connectionString, "data-protection")
+        //         .GetBlobClient("keys.xml");
+        //
+        builder.Services.AddDataProtection();
+        //         .PersistKeysToAzureBlobStorage(blobClient)
+        //         .SetApplicationName("Admitto");
 
             builder.Services.AddScoped<ITeamConfigEncryptionService, TeamConfigEncryptionService>();
-        }
+        // }
 
         return builder;
     }
