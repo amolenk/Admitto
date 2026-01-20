508ce577f08491cc9f41ad7c0124f823c521bf5f
Sander Molenkamp <sander@sandermolenkamp.com>
2026-01-20 09:29:17 +0100

    Update UI

--- src/Admitto.UI.Admin/app/components/nav-events.tsx
+++ src/Admitto.UI.Admin/app/components/nav-events.tsx
@@ -1,5 +1,6 @@
 "use client"
 
+import {TicketedEventDto} from "@/lib/admitto-api/generated";
 import { useRouter } from "next/navigation"
 
 import { SquarePlus, } from "lucide-react"
@@ -11,38 +12,72 @@ import {
     SidebarMenuItem,
     useSidebar,
 } from "@/components/ui/sidebar"
+import {useEffect, useState} from "react";
 
 export function NavEvents({
-                              teamId,
-                              events,
+                              teamSlug,
+                              // events,
                           }: {
-    teamId: string,
-    events: {
-        id: string
-        name: string
-    }[]
+    teamSlug: string,
+    // events: {
+    //     id: string
+    //     name: string
+    // }[]
 }) {
     const {isMobile} = useSidebar()
     const router = useRouter()
+
+    const [events, setEvents] = useState<Array<TicketedEventDto>>([]);
+
+    useEffect(() =>
+    {
+        async function fetchEvents()
+        {
+            console.log("Fetching events for team slug:", teamSlug);
+
+            try
+            {
+                console.log(`/api/teams/${teamSlug}/events`)
+
+                const response = await fetch(`/api/teams/${teamSlug}/events`, { method: "GET" });
+                if (!response.ok)
+                {
+                    throw new Error("Failed to fetch events");
+                }
+
+                const data = (await response.json()) as Array<TicketedEventDto>;
+                console.log(data)
+                setEvents(data);
+            }
+            catch (error)
+            {
+                console.error("Error fetching events:", error);
+            }
+
+            router.push("/")
+        }
+
+        fetchEvents();
+    }, [router]);
 
     return (
         <SidebarGroup className="group-data-[collapsible=icon]:hidden">
             <SidebarGroupLabel>Events</SidebarGroupLabel>
             <SidebarMenu>
-                {events.map((item) => (
-                    <SidebarMenuItem key={item.id}>
+                {events.map((ticketedEvent) => (
+                    <SidebarMenuItem key={ticketedEvent.slug}>
                         <SidebarMenuButton
                             asChild
-                            onClick={() => router.push(`/teams/${teamId}/events/${item.id}`)}
+                            onClick={() => router.push(`/teams/${teamSlug}/events/${ticketedEvent.slug}`)}
                         >
-                            <a href="#">{item.name}</a>
+                            <a href="#">{ticketedEvent.name}</a>
                         </SidebarMenuButton>
                     </SidebarMenuItem>
                 ))}
                 <SidebarMenuItem>
                     <SidebarMenuButton
                         className="text-sidebar-foreground/70"
-                        onClick={() => router.push(`/teams/${teamId}/events/add`)}
+                        onClick={() => router.push(`/teams/${teamSlug}/events/add`)}
                     >
                         <SquarePlus className="text-sidebar-foreground/70"/>
                         <a href="#">New event</a>
--- src/Admitto.UI.Admin/app/lib/auth.ts
+++ src/Admitto.UI.Admin/app/lib/auth.ts
@@ -1,59 +1,44 @@
-import { betterAuth } from "better-auth";
-import { genericOAuth, microsoftEntraId } from "better-auth/plugins"
-import { Pool } from "pg";
+import {betterAuth} from "better-auth";
+import {genericOAuth} from "better-auth/plugins"
+import {Pool} from "pg";
 
 export const auth = betterAuth({
     database: new Pool({
-        connectionString: ""
+        connectionString: process.env.NEXT_BETTER_AUTH
     }),
-    experimental: { joins: true },
+    experimental: {joins: true},
     plugins: [
         genericOAuth({
             config: [
-
+                {
+                    providerId: "generic-oauth",
+                    discoveryUrl: `${process.env.BETTER_AUTH_AUTHORITY || (() => {
+                        throw new Error("BETTER_AUTH_DISCOVERY_URL not set")
+                    })()}/.well-known/openid-configuration`,
+                    clientId: process.env.BETTER_AUTH_CLIENT_ID || (() => {
+                        throw new Error("BETTER_AUTH_CLIENT_ID not set")
+                    })(),
+                    clientSecret: process.env.BETTER_AUTH_CLIENT_SECRET || (() => {
+                        throw new Error("BETTER_AUTH_CLIENT_SECRET not set")
+                    })(),
+                    scopes: (process.env.BETTER_AUTH_SCOPES || (() => {
+                        throw new Error("BETTER_AUTH_SCOPES not set")
+                    })()).split(" "),
+                    prompt: process.env.BETTER_AUTH_PROMPT as "select_account" | "login" | undefined || (() => {
+                        throw new Error("BETTER_AUTH_PROMPT not set")
+                    })(),
+                }
             ]
         }),
     ],
-    // account: {
-    //     accountLinking: {
-    //         enabled: false,
-    //         allowDifferentEmails: true
-    //     },
-    // },
-    advanced: {
-        cookies: {
-            state: {
-                attributes: {
-                    sameSite: "none",
-                    secure: true,
-                }
+    cookies: {
+        state: {
+            attributes: {
+                sameSite: "none",
+                secure: true,
             }
-        },
-        // useSecureCookies: true,
-        // crossSubDomainCookies: {
-        //     enabled: false,
-        // },
-        // defaultCookieAttributes: {
-        //     sameSite: "none",
-        //     secure: true,
-        //     domain: undefined,
-        //     path: "/",
-        // },
-        // cookiePrefix: "better_auth",
-        // disableCSRFCheck: true,
+        }
     },
 });
-
-
-
-// providerId: 'okta',
-//     clientId: process.env.AUTH_OKTA_ID as string,
-//     clientSecret: process.env.AUTH_OKTA_SECRET as string,
-//     scopes: ['openid', 'profile', 'email', 'offline_access', 'groups'],
-//     discoveryUrl: `${process.env.AUTH_OKTA_ISSUER}/.well-known/openid-configuration`,
-//     accessType: 'offline',
-
-
-
 
 export type Session = typeof auth.$Infer.Session
